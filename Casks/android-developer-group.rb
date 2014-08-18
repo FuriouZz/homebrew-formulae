c55b637cbad74e513045863d6fc5ba9bf26b4719
Nicolas Brousse <nicolas-brousse@users.noreply.github.com>
2014-08-18 23:58:09 +0200

    Update android-developer-group.rb

--- Casks/android-developer-group.rb
+++ Casks/android-developer-group.rb
@@ -1,6 +1,8 @@
 class AndroidDeveloperGroup < Cask
   depends_on cask: 'commons-group'
   depends_on cask: 'developer-group'
+  
+  depends_on cask: 'android-file-transfer'
 
   # IDE
   depends_on cask: 'android-studio-bundle'
